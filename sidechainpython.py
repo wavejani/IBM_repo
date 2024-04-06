e280780b199b52553e10a6fb5b8433d32cc40010
wavejani <wavejani@users.noreply.github.com>
2024-04-06 17:10:27 +0300

    Create sidechainpython.py
    New python code.

---
+++ sidechainpython.py
@@ -0,0 +1,9 @@
+def greet():
+    # Prompting the user to enter their name
+    name = input("What is your name? ")
+
+    # Greeting the user
+    print("Hello, " + name + "! Welcome.")
+
+# Calling the greet function to execute the code
+greet()
